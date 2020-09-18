19eb68adb9fdea2a02fdf93cd05d3875bbf298f7
Ron Nielsen <ronaldnielsen@gmail.com>
2020-09-18 02:41:44 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.35eaa2b6132e266d689ee1055c0c33bf.js"
+  "/portfolio/precache-manifest.7a9fe9078b31d7963f7572e544165e1f.js"
 );
 
 self.addEventListener('message', (event) => {
