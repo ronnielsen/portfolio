4c1685dfe36144450ac5ce862b1b624e6c9a13f0
Ron Nielsen <ronaldnielsen@gmail.com>
2020-09-18 02:40:28 -0700

    Updates

--- precache-manifest.35eaa2b6132e266d689ee1055c0c33bf.js
+++ precache-manifest.35eaa2b6132e266d689ee1055c0c33bf.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f6a09c0c8fc0664473c623f6ee74068f",
+    "revision": "467d6d26dfdbc92c86e95703a422c379",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "da08bca64d6b5c46bca9",
+    "revision": "f0da6e43a0d7ff4e66bb",
     "url": "/portfolio/static/css/main.b18fd6ed.chunk.css"
   },
   {
-    "revision": "c5d5aaeeffb39caa7e50",
-    "url": "/portfolio/static/js/2.7559f39c.chunk.js"
+    "revision": "ce38029d505d24126a01",
+    "url": "/portfolio/static/js/2.55eb3b8a.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/portfolio/static/js/2.7559f39c.chunk.js.LICENSE.txt"
+    "url": "/portfolio/static/js/2.55eb3b8a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "da08bca64d6b5c46bca9",
-    "url": "/portfolio/static/js/main.97f86f2e.chunk.js"
+    "revision": "f0da6e43a0d7ff4e66bb",
+    "url": "/portfolio/static/js/main.cdbb928c.chunk.js"
   },
   {
     "revision": "7a6cc99bd8f08f9fa6ec",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.dc707715fc236fa26bb3de674c1580e0.js"
+  "/portfolio/precache-manifest.35eaa2b6132e266d689ee1055c0c33bf.js"
 );
 
 self.addEventListener('message', (event) => {
